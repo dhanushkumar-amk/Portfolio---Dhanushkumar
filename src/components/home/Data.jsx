90cf5ba8adfae0d1d1b9cafd9ae0b584f91182b0
Dhanushkumar G <145154541+dhanushkumar-amk@users.noreply.github.com>
2024-01-28 18:44:50 +0530

    Update Data.jsx

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -2,7 +2,7 @@ import React from 'react';const Data = () => {
   return (
     <div className='home__data'>
       <h1 className='home__title'>
-        Dhanush
+        Dhanush kumar G
         <svg
           width='36'
           height='36'
